ccf45541d2382366441a37b8cad078ddf2dac0ae
erlend <erlend.lee@qq.com>
2024-03-04 16:07:14 +0800

    新增内部GPT调用

--- ErlendTools.py
+++ ErlendTools.py
@@ -4,10 +4,60 @@ import re
 import datetime
 import sublime
 import sublime_plugin
+from sublime import Window, View
+from sublime_plugin import EventListener
+
 from .ErlendTRS import baidu_translate_api as my_trans
 
 
 TAG = '\n' + ('-' * 10) + '\n'
+
+
+class SharedOutputPanelListener(EventListener):
+    OUTPUT_PANEL_NAME = "GPT"
+
+    def __init__(self, window: Window = None, markdown: bool = True) -> None:
+        super().__init__()
+        self.markdown: bool = markdown
+        self.window: Window = window
+        self.output_panel: View = self.window.find_output_panel(
+            self.OUTPUT_PANEL_NAME) or self.window.create_output_panel(self.OUTPUT_PANEL_NAME)
+
+        if self.markdown:
+            self.output_panel.set_syntax_file(
+                "Packages/Markdown/MultiMarkdown.sublime-syntax")
+
+    def toggle_overscroll(self, enabled: bool):
+        self.output_panel.settings().set("scroll_past_end", enabled)
+
+    def update_output_panel(self, text: str):
+        self.output_panel.set_read_only(False)
+        self.output_panel.run_command('append', {'characters': text})
+        self.output_panel.set_read_only(True)
+
+    def refresh_output_panel(self):
+        self.output_panel.set_read_only(False)
+        # scrolling panel to the bottom.
+        point = self.output_panel.text_point(
+            __get_number_of_lines__(view=self.output_panel), 0)
+        self.output_panel.show_at_center(point)
+        self.output_panel.set_read_only(True)
+
+    def clear_output_panel(self):
+        self.show_panel()
+        self.output_panel.set_read_only(False)
+        # self.output_panel.erase(edit, sublime.Region(0, self.output_panel.size()))
+        self.output_panel.run_command("select_all")
+        self.output_panel.run_command("right_delete")
+
+    def show_panel(self):
+        self.window.run_command(
+            "show_panel", {"panel": f"output.{self.OUTPUT_PANEL_NAME}"})
+
+
+def __get_number_of_lines__(view: View) -> int:
+    last_line_num = view.rowcol(view.size())[0]
+    return last_line_num
 
 
 def get_trans_result(r_str):
@@ -20,32 +70,15 @@ def get_trans_result(r_str):
     return '\n'.join(text) + '\n'
 
 
-class TransToEnglish(sublime_plugin.TextCommand):
-    def run(self, edit):
+class Translate(sublime_plugin.TextCommand):
+    def run(self, edit, to_lang):
         selection = self.view.sel()
         old_text = self.view.substr(selection[0])
 
         query = old_text
 
         from_lang = 'auto'
-        to_lang = 'en'
-
-        r_str = my_trans(from_lang, to_lang, query)
-
-        new_text = TAG + get_trans_result(r_str)
-        self.view.insert(edit, max(selection[0].a, selection[0].b), new_text)
-
-
-class TransToChinese(sublime_plugin.TextCommand):
-    def run(self, edit):
-        selection = self.view.sel()
-        old_text = self.view.substr(selection[0])
-
-        query = old_text
-
-        from_lang = 'auto'
-        to_lang = 'zh'
-
+        # to_lang en 英文；zh 中文
         r_str = my_trans(from_lang, to_lang, query)
 
         new_text = TAG + get_trans_result(r_str)
@@ -130,12 +163,13 @@ class ShiftVimModel(sublime_plugin.ApplicationCommand):
         s = sublime.load_settings("Preferences.sublime-settings")
         current = s.get("ignored_packages", [])
         msg = ''
+        vim_name = "Vintage"
 
-        if current:
-            current = []
+        if vim_name in current:
+            current.remove(vim_name)
             msg = 'Enable vim MODEL'
         else:
-            current = ["Vintage"]
+            current.append(vim_name)
             msg = 'Unable vim MODEL'
 
         s.set("ignored_packages", current)
@@ -149,6 +183,7 @@ class ShiftCamelOrUnderline(sublime_plugin.TextCommand):
         selection = self.view.sel()
         region = sublime.Region(selection[0].a, selection[0].b)
         old_text = self.view.substr(selection[0])
+
         # 下划线转驼峰
         if '_' in old_text:
             words = old_text.split('_')
@@ -168,3 +203,64 @@ class ShiftCamelOrUnderline(sublime_plugin.TextCommand):
                     else:
                         result += char
         self.view.replace(edit, region, result)
+
+
+import threading
+import requests
+import json
+
+
+class AskGpt(sublime_plugin.TextCommand):
+    def __init__(self, view):
+        super().__init__(view)
+        self.window = sublime.active_window()
+        self.listener = SharedOutputPanelListener(
+            window=self.window, markdown=True)
+
+    def run(self, edit, panel_op='ask'):
+        if panel_op == 'ask':
+            thread = threading.Thread(target=self.request_gpt)
+            thread.start()
+        elif panel_op == 'just_show':
+            self.listener.show_panel()
+        elif panel_op == 'clear':
+            self.listener.clear_output_panel()
+
+    def get_prompt(self):
+        selection = self.view.sel()
+        prompt = self.view.substr(selection[0])
+        return prompt
+
+    def output_panel(self, text):
+        self.listener.update_output_panel(text)
+        self.listener.refresh_output_panel()
+
+    def request_gpt(self):
+        self.listener.show_panel()
+        self.listener.toggle_overscroll(enabled=False)
+
+        url = "http://gpt.longi.com:8080/api/chat/streamChatWithWeb/V3"
+        payload = json.dumps({
+            "conversationId": "82d80c79-125a-4b1f-b398-ae28b8969480",
+            "prompt": self.get_prompt(),
+            "options": {},
+            "model": "azure-gpt-4",
+            "contentNumber": 20,
+            "systemMessage": "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.",
+            "temperature": 0.8,
+            "top_p": 1
+        })
+        headers = {
+            'blueCat_token': '????',
+            'Content-Type': 'application/json'
+        }
+
+        self.output_panel(
+            text="\n\n--------------------------开始请求--------------------------\n\n")
+        with requests.request("POST", url, headers=headers, data=payload, stream=True) as response:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    self.output_panel(chunk.decode('utf-8'))
+
+        sublime.set_timeout(self.output_panel(
+            "\n\n\n--------------------------请求完成--------------------------"), 0)
